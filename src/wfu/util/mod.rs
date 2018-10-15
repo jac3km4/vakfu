fb87585706c8d8963d88fc44fd167493b8aecd47
AG-w <qoop272@gmail.com>
2018-10-16 03:23:21 +0800

    various fix
    soft light
    float
    filter

--- src/wfu/util/mod.rs
+++ src/wfu/util/mod.rs
@@ -14,18 +14,18 @@ pub fn first_greater_power_of_two(value: i32) -> i32 {
     v + 1
 }
 
-pub fn iso_to_screen_x(iso_local_x: i32, iso_local_y: i32) -> i32 {
-    (iso_local_x - iso_local_y) * 43 // * 86 * 0.5f
+pub fn iso_to_screen_x(iso_local_x: i32, iso_local_y: i32) -> f32 {
+    (iso_local_x - iso_local_y) as f32 * 43f32 // 43 * 86 * 0.5f
 }
 
-pub fn iso_to_screen_y(iso_local_x: i32, iso_local_y: i32, iso_altitude: i32) -> i32 {
-    (-(iso_local_x + iso_local_y) as f32 * 21.5f32) as i32 + iso_altitude * 10
+pub fn iso_to_screen_y(iso_local_x: i32, iso_local_y: i32, iso_altitude: i32) -> f32 {
+    (-(iso_local_x + iso_local_y) as f32 * 21.5f32) + iso_altitude as f32 * 10f32 //21.5
 }
 
-pub fn screen_to_iso_x(x: f32, y: f32) -> f32 {
-    x / 86f32 - y / 43f32
+pub fn screen_to_iso_x(x: f32, y: f32) -> i32 {
+    (x / 86f32 - y / 43f32) as i32
 }
 
-pub fn screen_to_iso_y(x: f32, y: f32) -> f32 {
-    -(x / 86f32 + y / 43f32)
+pub fn screen_to_iso_y(x: f32, y: f32) -> i32 {
+    -(x / 86f32 + y / 43f32) as i32
 }
